f8d7bb0dcdca73ffe90db4b8637428f56795f4b4
AlejandroEchav <echavarriaaj@gmail.com>
2017-10-13 09:54:04 -0600

    Third attempt
    This is a third attempt at GitHub and Eclipse linking

--- GitTester/src/GitTest.java
+++ GitTester/src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("This is a test");
 		System.out.println("This is another test, in order to demonstrate a change");
+		System.out.println("Yes No Yes No");
 	}
 
 }
